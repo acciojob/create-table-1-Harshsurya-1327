ea8446174fcd51e2aff8e2a665d8145a7fee5c41
Harshsurya-1327 <harshsurya1327@gmail.com>
2025-09-27 14:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function insert_Row() {
-    //Write your code here
-  
-  
+function insert_row() {
+    // Get the table
+    var table = document.getElementById("myTable");
+
+    // Insert a new row at the end
+    var newRow = table.insertRow(-1);
+
+    // Insert 2 cells
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    // Fill cells with text
+    cell1.innerHTML = "New Cell 1";
+    cell2.innerHTML = "New Cell 2";
 }
